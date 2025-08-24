ea717e7e5b4dd6556b053c9a13082c93488d3719
Vinaylingala5 <vinaybabulingala@gmail.com>
2025-08-24 15:12:49 +0530

    intial commit

---
+++ src/main/java/com/vinay/sync_up/SyncUp/SyncUpApplication.java
@@ -0,0 +1,13 @@
+package com.vinay.sync_up.SyncUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinay/sync_up/SyncUp/SyncUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinay.sync_up.SyncUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
